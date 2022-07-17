3f63ad12f627fc08f7362e8cea889f3f6fb57e6a
unknown <afifmakarim88@gmail.com>
2022-07-17 09:51:01 +0700

    delete unused variable

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,7 +2,6 @@ import React from "react";
 import Layout from "../../components/Layout/Layout";
 import SideBar from "../../components/SideBar/SideBar";
 import { Outlet } from "react-router-dom";
-import Content from "../../components/Content/Content";
 
 export default function Dashboard() {
   return (
